06ac8e11061058a42b79225cd229d48a0d0afb39
Wilton Jr <wilton007@hotmail.com>
2023-01-02 11:04:41 -0300

    Exercicios 1001

---
+++ beecrowd/src/exercicios/Exercicio_1001.java
@@ -0,0 +1,15 @@
+package exercicios;
+
+import java.util.Scanner;
+
+public class Exercicio_1001 {
+    public static void main(String[] args) {
+        int A,B, resultado = 0;
+        Scanner entrada = new Scanner(System.in);
+        A = entrada.nextInt();
+        B = entrada.nextInt();
+        resultado = A + B;
+        System.out.println("X = "+resultado);
+
+    }
+}
