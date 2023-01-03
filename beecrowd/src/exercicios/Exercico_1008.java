1a4bc3f215d01a69cc8e28cb910cfe5fe30d53f2
Wilton Jr <wilton007@hotmail.com>
2023-01-03 09:26:09 -0300

    Exercicios 1008

---
+++ beecrowd/src/exercicios/Exercico_1008.java
@@ -0,0 +1,20 @@
+package exercicios;
+
+import java.text.DecimalFormat;
+import java.util.Scanner;
+
+public class Exercico_1008 {
+    public static void main(String[] args) {
+        DecimalFormat df = new DecimalFormat("00.00");
+        Scanner entrada = new Scanner(System.in);
+
+        int numerofuncionario = entrada.nextInt();
+        int horastrabalhadas = entrada.nextInt();
+        float valorhora = entrada.nextFloat();
+
+        float salario = horastrabalhadas * valorhora;
+
+        System.out.println("NUMBER = " + numerofuncionario);
+        System.out.println("SALARY = U$ " + df.format(salario));
+    }
+}
