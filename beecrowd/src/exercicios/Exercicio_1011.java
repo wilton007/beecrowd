f773317d3806bcb1c9bf1c0d40b00867e73b81bd
Wilton Jr <wilton007@hotmail.com>
2023-01-03 10:25:05 -0300

    Exercicio 1011

---
+++ beecrowd/src/exercicios/Exercicio_1011.java
@@ -0,0 +1,17 @@
+package exercicios;
+
+import java.text.DecimalFormat;
+import java.util.Scanner;
+
+public class Exercicio_1011 {
+    public static void main(String[] args) {
+        DecimalFormat df = new DecimalFormat("00.000");
+        Scanner entrada = new Scanner(System.in);
+
+        double R = entrada.nextDouble();
+
+        double volume = (4 / 3.0) * 3.14159 * (R * R * R);
+
+        System.out.println("VOLUME = " + df.format(volume));
+    }
+}
