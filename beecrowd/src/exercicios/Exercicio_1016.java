d05fc1e7d3cda5fd4fcabb72e398b28320b1666c
Wilton Jr <wilton007@hotmail.com>
2023-01-04 08:48:48 -0300

    Exercico 1016

---
+++ beecrowd/src/exercicios/Exercicio_1016.java
@@ -0,0 +1,13 @@
+package exercicios;
+
+import java.util.Scanner;
+
+public class Exercicio_1016 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        int x = input.nextInt();
+        x = x * 2;
+        System.out.println(x + " minutos");
+    }
+}
