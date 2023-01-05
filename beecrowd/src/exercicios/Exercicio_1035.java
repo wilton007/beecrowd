bb7be17fe441bb10f5ca7ccd609ab8942ac5256f
Wilton Jr <wilton007@hotmail.com>
2023-01-05 09:08:15 -0300

    Exercicios 1035

---
+++ beecrowd/src/exercicios/Exercicio_1035.java
@@ -0,0 +1,11 @@
+package exercicios;
+
+import java.util.Scanner;
+
+public class Exercicio_1035 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+
+    }
+}
